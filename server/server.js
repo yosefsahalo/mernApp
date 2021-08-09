4b220b1c0b7ae1cb325384216dff726de7fa4bdf
yosefsahalo <yosefsahalu@gmail.com>
2021-08-09 16:56:30 +0300

    final changes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const {MongoClient,ObjectId} = require('mongodb');
 const dotenv = require('dotenv')
 dotenv.config();
-
-const PORT = process.env.PORT
+const path = require('path')
+const PORT = process.env.PORT || 8080;
 const cors = require('cors')
 const studentRouter = require('./routers/studentRouter')
 const dbCoonection = require('./DB/index')
@@ -19,7 +19,18 @@ app.listen(PORT,()=>{
     console.log("Listen On Port 8080");
 })
 
-app.get('/',(req,res)=>{
+app.get('/API',(req,res)=>{
     res.send("Listen On Port 8080");
     // connectAndAdd(res)
 })
+
+//***********************************************************************/
+if(process.env.NODE_ENV === 'production'){
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    // Handle React routing 
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname, '../client/build','index.html'))
+    })
+}
+//***********************************************************************/
