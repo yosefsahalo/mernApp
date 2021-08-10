ef21f4044dfe39521f32d96859b01aba688f0bce
yosefsahalo <yosefsahalu@gmail.com>
2021-08-10 10:11:57 +0300

    change the package.json

--- server/server.js
+++ server/server.js
@@ -1,28 +1,22 @@
-const express = require('express');
-const {MongoClient,ObjectId} = require('mongodb');
 const dotenv = require('dotenv')
 dotenv.config();
+const express = require('express');
 const path = require('path')
 const PORT = process.env.PORT || 8080;
 const cors = require('cors')
 const studentRouter = require('./routers/studentRouter')
 const dbCoonection = require('./DB/index')
+
 const app = express();
-app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// dbCoonection.on('error',()=>{console.log('Connection Error')})
-
-app.use('/API/students',studentRouter)
+app.use(cors())
 
 app.listen(PORT,()=>{
     console.log("Listen On Port 8080");
 })
 
-app.get('/API',(req,res)=>{
-    res.send("Listen On Port 8080");
-    // connectAndAdd(res)
-})
+app.use('/API/students',studentRouter)
 
 //***********************************************************************/
 if(process.env.NODE_ENV === 'production'){
